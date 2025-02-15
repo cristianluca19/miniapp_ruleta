dde7cddd3f331ff135fd94a5993003bbdd03140b
cristianluca19 <cristianlucatti@gmail.com>
2025-02-15 13:12:19 -0300

    Test in app with console

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,20 +2,17 @@
 import "@worldcoin/mini-apps-ui-kit-react/styles.css";
 import NextAuthProvider from "@/components/NextAuthProvider";
 import MiniKitProvider from "@/components/minikit-provider";
-// import { ErudaProvider } from "@/components/Eruda";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
-      {/* <ErudaProvider> */}
-        <MiniKitProvider>
-          <NextAuthProvider>
-            <body>
-              {children}
-            </body>
-          </NextAuthProvider>
-        </MiniKitProvider>
-      {/* </ErudaProvider> */}
+      <MiniKitProvider>
+        <NextAuthProvider>
+          <body>
+            {children}
+          </body>
+        </NextAuthProvider>
+      </MiniKitProvider>
     </html>
   );
 }
